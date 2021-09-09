d84f338114b37560410a50b254487844464be55a
Rdf15 <rodrigo_f15@outlook.com.br>
2021-09-09 18:49:41 -0300

    mais desafios

---
+++ src/DesafiosJavaBasico/DesafioTheon.java
@@ -0,0 +1,22 @@
+package DesafiosJavaBasico;
+
+import java.util.Scanner;
+
+public class DesafioTheon {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        int T, menor = Integer.MAX_VALUE, posMenor = 0;
+
+        for (int i = 0; i < N; ++i) {
+            T = leitor.nextInt();
+            if (T < menor) {
+                menor = T;
+                posMenor = i+1;
+            }
+        }
+        System.out.println(posMenor);
+    }
+}
+
---
+++ src/DesafiosJavaBasico/Divisores.java
@@ -0,0 +1,23 @@
+package DesafiosJavaBasico;
+
+import java.util.Scanner;
+
+public class Divisores {
+
+        //complete os espaços em branco com sua solução
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            int n;
+            n = input.nextInt();
+
+            for (int i = 1;i<=n; i++) {
+                if (n % i == 0) {
+                    System.out.println(  i     );
+                }
+            }
+            System.out.println(        );
+        }
+
+    }
+
+
